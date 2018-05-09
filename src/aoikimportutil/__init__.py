18a768eb8ab5ff4f1ba8fcaeed617ca928df63cb
Aoik <aoi.kuiyuyou@gmail.com>
2018-05-10 01:05:45 +0800

    Refactor code

--- src/aoikimportutil/__init__.py
+++ src/aoikimportutil/__init__.py
@@ -1,4 +1,18 @@
 # coding: utf-8
-from __future__ import absolute_import ## require Python 2.5+
-from aoikimportutil.aoikimportutil import *
-from aoikimportutil.aoikimportutil import __version__
+from __future__ import absolute_import  # Require Python 2.5+
+
+import aoikimportutil.aoikimportutil as _aoikimportutil
+
+
+__version__ = _aoikimportutil.__version__
+
+
+# Support usage like:
+# `from aoikimportutil import load_obj`
+# instead of:
+# `from aoikimportutil.aoikimportutil import load_obj`
+#
+for key in getattr(_aoikimportutil, '__all__'):
+    globals()[key] = getattr(_aoikimportutil, key)
+
+del _aoikimportutil
